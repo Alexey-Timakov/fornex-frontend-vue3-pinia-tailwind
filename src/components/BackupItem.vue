e74ec42923fc11405e033e7f5d250b14ed708041
WIN-18FGUV8VB5F\alix <avtimakov70@gmail.com>
2024-07-19 00:03:34 +0300

    Add Backup component, icons

---
+++ src/components/BackupItem.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { TBackup } from '@/helpers/fetchData'
+import DataIcon from '../icons/Data.svg'
+import RemoveIcon from '../icons/Remove.svg'
+import RestoreIcon from '../icons/Restore.svg'
+
+interface IBackupItem {
+  item: TBackup
+}
+
+const { item } = defineProps<IBackupItem>()
+</script>
+
+<template>
+  <div
+    class="grid grid-cols-[3rem_3rem_1fr_7rem_15rem_3rem_3rem] mb-4 text-2xl font-thin items-center justify-items-stretch *:p-2 border-b-[0.1rem] *:border-r-[0.1rem]"
+  >
+    <span class="w-12 h-12 border-l-[0.1rem]">
+      <DataIcon />
+    </span>
+    <p class="text-center">{{ item.id }}</p>
+    <p>{{ item.title }}</p>
+    <p class="text-center">{{ item.size_mb }} Mb</p>
+    <p class="text-center">{{ new Date(+item.date).toLocaleString() }}</p>
+    <span class="w-12 h-12">
+      <RestoreIcon class="cursor-pointer" />
+    </span>
+    <span class="w-12 h-12">
+      <RemoveIcon class="cursor-pointer" />
+    </span>
+  </div>
+</template>

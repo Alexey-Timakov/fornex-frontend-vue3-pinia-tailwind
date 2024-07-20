a9c30b4c8e1d05e125b521ad50588cfa4479a8cb
WIN-18FGUV8VB5F\alix <avtimakov70@gmail.com>
2024-07-20 21:14:30 +0300

    Add dist, add version status to Sidebar

--- src/layout/LayoutSidebar.vue
+++ src/layout/LayoutSidebar.vue
@@ -4,13 +4,16 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <nav class="grid grid-cols-1 items-start gap-8 p-4 text-2xl text-slate-500">
-    <RouterLink
-      active-class="text-slate-700 font-bold"
-      v-for="r in routes"
-      :key="r.path"
-      :to="r.path"
-      >{{ r.name }}</RouterLink
-    >
-  </nav>
+  <div class="grid grid-rows-[1fr_auto] h-full items-start">
+    <nav class="grid grid-cols-1 items-start gap-8 p-4 text-2xl text-slate-500">
+      <RouterLink
+        active-class="text-slate-700 font-bold"
+        v-for="r in routes"
+        :key="r.path"
+        :to="r.path"
+        >{{ r.name }}</RouterLink
+      >
+    </nav>
+    <span>v.0.2</span>
+  </div>
 </template>
